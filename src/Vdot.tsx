5627602a26ccf61d1f479c9617e82e0e0fc8c735
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-10-19 23:12:53 -0400

    added navigation back home

--- src/Vdot.tsx
+++ src/Vdot.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 const Vdot = () => {
   return (
+    <>
+          <div className="flex lg:flex-1">
+            <a href="/" className="-m-1.5 p-1.5 flex items-center justify-between p-6 lg:px-8">
+              <span className="sr-only">Marathon Man</span>
+              <img
+                className="h-8 w-auto"
+                src="https://www.svgrepo.com/show/158299/runner.svg"
+                alt=""
+              />
+            </a>
+            </div>
     <div className='mx-auto max-w-2xl py-32 sm:py-24 lg:py-48'>
         <div className='text-center'>
             <h1 className='text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
@@ -12,6 +23,7 @@ const Vdot = () => {
             </div>
         </div>
     </div>
+    </>
   )
 }
 
